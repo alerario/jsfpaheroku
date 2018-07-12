8e70eb76ad21788ce983ba930378ee5529a91867
Alexandre L'Erario <alerario@gmail.com>
2018-07-12 17:29:58 -0300

    Criar procedimento para configurar banco no heroku

--- src/main/java/br/web/JSFHerokuBDSetup.java
+++ src/main/java/br/web/JSFHerokuBDSetup.java
@@ -25,37 +25,65 @@ public class JSFHerokuBDSetup {
      * Creates a new instance of JSFHerokuBDSetup
      */
     private String conf;
-    
-    public JSFHerokuBDSetup() {
-    }
-    
-    public String getConf(){
-        return conf;
-    }
+    private String host;
     
 
+    public JSFHerokuBDSetup() {
+    }
+
+    public String getConf() {
+        return conf;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    
     public void configurar() {
         String databaseUrl = System.getenv("DATABASE_URL");
-        this.conf = databaseUrl;
-        StringTokenizer st = new StringTokenizer(databaseUrl, ":@/");
-        String dbVendor = st.nextToken(); //if DATABASE_URL is set
-        String userName = st.nextToken();
-        String password = st.nextToken();
-        String host = st.nextToken();
-        String port = st.nextToken();
-        String databaseName = st.nextToken();
-        String jdbcUrl = String.format("jdbc:postgresql://%s:%s/%s?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", host, port, databaseName);
-        Map<String, String> properties = new HashMap<String, String>();
-        properties.replace("javax.persistence.jdbc.url", databaseUrl);
-        properties.replace("javax.persistence.jdbc.user", userName);
-        properties.replace("javax.persistence.jdbc.password", password);
-        properties.replace("javax.persistence.jdbc.driver", "org.postgresql.Driver");
-       // properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory(br.data.crud.EMNames.EMN1, properties);
-        
-      //  <property name="javax.persistence.jdbc.url" value="jdbc:postgresql://localhost:5432/cfdb"/>
-      //<property name="javax.persistence.jdbc.user" value="postgres"/>
-      //<property name="javax.persistence.jdbc.driver" value="org.postgresql.Driver"/>
-     // <property name="javax.persistence.jdbc.password" value="postgres"/>
+        if (databaseUrl != null) {
+            this.conf = databaseUrl;
+            StringTokenizer st = new StringTokenizer(databaseUrl, ":@/");
+            String dbVendor = st.nextToken(); //if DATABASE_URL is set
+            String userName = st.nextToken();
+            String password = st.nextToken();
+             host = st.nextToken();
+            String port = st.nextToken();
+            String databaseName = st.nextToken();
+            String jdbcUrl = String.format("jdbc:postgresql://%s:%s/%s?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", host, port, databaseName);
+            Map<String, String> properties = new HashMap<String, String>();
+            properties.put("javax.persistence.jdbc.url", jdbcUrl);
+            properties.put("javax.persistence.jdbc.user", userName);
+            properties.put("javax.persistence.jdbc.password", password);
+            properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+            // properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory(br.data.crud.EMNames.EMN1, properties);
+            emf.createEntityManager();
+        } else {
+           
+            String dbVendor = "jdbc:postgresql://localhost:5432/cfdb";
+            String userName = "postgres";
+            String password = "postgres";
+             host = "localhost";
+            String port = "5432";
+            String databaseName = "cfdb";
+            String jdbcUrl = "jdbc:postgresql://localhost:5432/cfdb";
+            this.conf = jdbcUrl;
+            Map<String, String> properties = new HashMap<String, String>();
+            properties.put("javax.persistence.jdbc.url", jdbcUrl);
+            properties.put("javax.persistence.jdbc.user", userName);
+            properties.put("javax.persistence.jdbc.password", password);
+            properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+            // properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory(br.data.crud.EMNames.EMN1, properties);
+            emf.createEntityManager();
+        }
+        //removido do arquivo persistence.xml
+
+        //  <property name="javax.persistence.jdbc.url" value="jdbc:postgresql://localhost:5432/cfdb"/>
+        //<property name="javax.persistence.jdbc.user" value="postgres"/>
+        //<property name="javax.persistence.jdbc.driver" value="org.postgresql.Driver"/>
+        // <property name="javax.persistence.jdbc.password" value="postgres"/>
     }
 }
